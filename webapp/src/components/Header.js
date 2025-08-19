4e867c7d6ff5bd51e2304806494355615dcae848
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2025-08-19 15:29:39 -0500

    Update Header.js

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ReactComponent as RegulationLogo } from '../logo.svg';
 import GlobalSearchBar from './GlobalSearchBar';
 
